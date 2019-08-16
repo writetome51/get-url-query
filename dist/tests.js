ee21cfffbb501c94890d5cf89be42c95dbd7dfa7
Steve Thompson <writetome51@gmail.com>
2019-08-16 14:28:39 -0600

    minor change

---
+++ dist/tests.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var index_1 = require("./index");
+var a = 22;
+var k = 1;
+var result = index_1.getURLQuery({ k: k, a: a });
+console.log(result);
---
+++ lib/tests.ts
@@ -0,0 +1,6 @@
+import { getURLQuery } from './index';
+
+let a = 22;
+let k = 1;
+let result = getURLQuery({k, a});
+console.log(result);
